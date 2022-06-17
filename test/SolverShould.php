51e036c6bbb4c1b59fce366617e1d4810eda8cf1
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-06-17 11:26:02 +0200

    Added test to verify Sudoku immutability

--- test/SolverShould.php
+++ test/SolverShould.php
@@ -22,10 +22,22 @@ class SolverShould extends TestCase
      * @test
      */
 
+    public function not_alter_original_sudoku_while_solving_it()
+    {
+        $matrix = $this->buildSolvableSudokuMatrix();
+        $sudoku = new Sudoku($matrix);
+        $this->solver->getSolutionFor($sudoku);
+
+        $this->assertTrue($this->sudokuEqualsMatrix($sudoku, $matrix));
+    }
+    /**
+     * @test
+     */
+
     public function detect_whether_a_sudoku_is_solvable_or_not()
     {
-        $solvableSudoku = $this->buildSolvableSudoku();
-        $unsolvableSudoku = $this->buildUnsolvableSudoku();
+        $solvableSudoku = new Sudoku($this->buildSolvableSudokuMatrix());
+        $unsolvableSudoku = new Sudoku($this->buildUnsolvableSudokuMatrix());
 
         $this->assertTrue($this->solver->isSolvable($solvableSudoku));
         $this->assertFalse($this->solver->isSolvable($unsolvableSudoku));
@@ -36,7 +48,7 @@ class SolverShould extends TestCase
 
     public function return_null_if_not_solvable()
     {
-        $unsolvableSudoku = $this->buildUnsolvableSudoku();
+        $unsolvableSudoku = new Sudoku($this->buildUnsolvableSudokuMatrix());
         $this->assertNull($this->solver->getSolutionFor($unsolvableSudoku));
     }
 
@@ -46,34 +58,51 @@ class SolverShould extends TestCase
      */
     public function return_correct_solution_if_solvable(): void
     {
-        $sudoku = $this->buildSolvableSudoku();
+        $sudoku = new Sudoku($this->buildSolvableSudokuMatrix());
         $solvedSudoku = $this->solver->getSolutionFor($sudoku);
 
         $this->assertTrue($solvedSudoku->isSolved());
     }
 
     /**
-     * @return Sudoku
+     * @return array
      */
-    private function buildUnsolvableSudoku(): Sudoku
+    private function buildUnsolvableSudokuMatrix(): array
     {
-        return new Sudoku(
+        return
             [
                 [1, 0, 3, 4],
                 [3, 4, 1, 2],
                 [0, 3, 2, 1],
                 [4, 1, 0, 3],
-            ]);
+            ];
     }
 
-    private function buildSolvableSudoku(): Sudoku
+    /**
+     * @return \int[][]
+     */
+    private function buildSolvableSudokuMatrix(): array
     {
-        return new Sudoku(
+        return
             [
                 [0, 2, 0, 4],
                 [3, 0, 1, 2],
                 [2, 3, 0, 1],
                 [4, 0, 2, 0],
-            ]);
+            ];
+    }
+
+    private function sudokuEqualsMatrix(Sudoku $sudoku, array $matrix): bool
+    {
+        for ($row = 0; $row < $sudoku->getRowCount(); $row++ ) {
+            for ($col = 0; $col < $sudoku->getRowCount(); $col++ ) {
+                if ($sudoku->getValueForSquare($row, $col) !== $matrix[$row][$col]) {
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
